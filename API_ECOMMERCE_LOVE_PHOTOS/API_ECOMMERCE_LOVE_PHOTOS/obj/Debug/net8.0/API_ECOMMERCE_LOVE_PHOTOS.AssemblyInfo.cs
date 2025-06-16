0ba31ec8b896091e45aca5a01b8406d136951119
evaldo <evaaldormj@gmail.com>
2025-06-16 20:45:23 -0300

    Update: Add new columns to the integration

--- API_ECOMMERCE_LOVE_PHOTOS/API_ECOMMERCE_LOVE_PHOTOS/obj/Debug/net8.0/API_ECOMMERCE_LOVE_PHOTOS.AssemblyInfo.cs
+++ API_ECOMMERCE_LOVE_PHOTOS/API_ECOMMERCE_LOVE_PHOTOS/obj/Debug/net8.0/API_ECOMMERCE_LOVE_PHOTOS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_ECOMMERCE_LOVE_PHOTOS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+660a65e5cd433803225966d647f0d136bdef2c4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6490b52a528b7c38c6be011b5587453fd9ff6297")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_ECOMMERCE_LOVE_PHOTOS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_ECOMMERCE_LOVE_PHOTOS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
