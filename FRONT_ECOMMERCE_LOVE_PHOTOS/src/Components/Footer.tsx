47206c662d97949e61da26725893bac4bf20ca8b
evaldo <evaaldormj@gmail.com>
2025-06-17 19:54:16 -0300

    Feat: Add responsive to footer component

--- FRONT_ECOMMERCE_LOVE_PHOTOS/src/Components/Footer.tsx
+++ FRONT_ECOMMERCE_LOVE_PHOTOS/src/Components/Footer.tsx
@@ -3,14 +3,14 @@ import { InstagramLogo } from "phosphor-react";
 export function Footer() {
     return (
         <footer id="footer" className="flex justify-around items-center py-8 border border-gray-200">
-            <div className="w-[600px] flex flex-col items-center gap-4 text-center">
+            <div className="hidden w-[600px] lg:flex flex-col items-center gap-4 text-center">
                 <h2 className="font-bold text-2xl">Atenção!</h2>
                 <p>Nossos dois principais meios de contato são via direct no <a className="font-bold underline" href="https://www.instagram.com/lovephotooficial/">Instagram</a> e via whatsapp. Escolha qual o atende mais!</p>
             </div>
             <div className="w-[600px] flex flex-col items-center gap-2 text-center">
                 <h2 className="font-bold text-2xl">Telefone para contato</h2>
-                <p>+55 (85) 99935-4328</p>
-                <a href="">
+                <p>+55 (85) 99612-8272</p>
+                <a href="https://www.instagram.com/lovephotooficial/">
                     <InstagramLogo size={100} />
                 </a>
             </div>
