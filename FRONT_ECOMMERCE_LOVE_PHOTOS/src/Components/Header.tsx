6d96fd011d925fa77c382ffeac568386de92b73a
evaldo <evaaldormj@gmail.com>
2025-06-16 22:04:24 -0300

    Feat: Add error div to products

--- FRONT_ECOMMERCE_LOVE_PHOTOS/src/Components/Header.tsx
+++ FRONT_ECOMMERCE_LOVE_PHOTOS/src/Components/Header.tsx
@@ -7,7 +7,7 @@ export function Header() {
     return (
         <header className="flex justify-around items-center py-6 w-full border-1 border-gray-200">
             <div className="flex justify-center items-center gap-20">
-                <h2 className="text-3xl font-semibold">Love Photos</h2>
+                <a href="#" className="text-3xl font-semibold">Love Photos</a>
                 <ul className="flex gap-6">
                     <li><a className="relative text-gray-700 after:absolute after:left-0 after:bottom-0 after:translate-y-1 after:h-[2px] after:w-full after:bg-gray-700 after:scale-x-0 after:origin-left after:transition-all after:duration-300 hover:after:scale-x-100" href="#product">Compre conosco</a></li>
                     <li><a className="relative text-gray-700 after:absolute after:left-0 after:bottom-0 after:translate-y-1 after:h-[2px] after:w-full after:bg-gray-700 after:scale-x-0 after:origin-left after:transition-all after:duration-300 hover:after:scale-x-100" href="#delivery">Entrega</a></li>
--- FRONT_ECOMMERCE_LOVE_PHOTOS/src/Components/Products.tsx
+++ FRONT_ECOMMERCE_LOVE_PHOTOS/src/Components/Products.tsx
@@ -4,6 +4,7 @@ import "slick-carousel/slick/slick-theme.css";
 import { Product } from "./Product";
 import type { ProductType } from "../Interfaces/ProductType";
 import { useEffect, useState } from "react";
+import { XCircle } from "phosphor-react";
 
 export function Products() {
     const settings = {
@@ -27,19 +28,37 @@ export function Products() {
     }, []);
 
     return (
-        <Slider {...settings}>
-            {products.map((product) => (
-                <Product
-                    key={product.id}
-                    Id={product.id}
-                    Name={product.name}
-                    Description={product.description}
-                    Price={product.price}
-                    Base64={product.base64}
-                    SellerName={product.sellerName}
-                    SellerPhone={product.sellerPhone}
-                />
-            ))}
-        </Slider>
+        <div>
+            { products.length ? (
+                <Slider {...settings}>
+                {products.map((product) => (
+                    <Product
+                        key={product.id}
+                        Id={product.id}
+                        Name={product.name}
+                        Description={product.description}
+                        Price={product.price}
+                        Base64={product.base64}
+                        SellerName={product.sellerName}
+                        SellerPhone={product.sellerPhone}
+                    />
+                ))}
+            </Slider>
+            ) :
+            <div className="flex flex-col items-center text-center pt-12 px-6 max-w-md mx-auto">
+                <XCircle size={150} className="text-red-600 mb-6 animate-pulse" />
+                <h1 className="text-3xl font-semibold mb-3 text-gray-800">Ops! Estamos enfrentando alguns problemas</h1>
+                <span className="mt-6">
+                    Enquanto isso, que tal dar uma passada no nosso{' '}
+                    <a 
+                        href="https://www.instagram.com/lovephotooficial/" 
+                        className="underline font-bold transition"
+                    >
+                    Instagram
+                    </a>?
+                </span>
+            </div>
+            }       
+        </div> 
     )
 }
